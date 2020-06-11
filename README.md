a206c1549d729345f84351145d20e675421f1121
Chridas <christinadaskalopoulou@gmail.com>
2020-06-11 21:57:25 +0300

    Uploading the files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Grocery List
+
+This is a demo project for a grocery list. This list was made with Vanilla JavaScript and the styling with CSS flexbox.
+
+The list is an ordered list and:
+
+1. You can add a new item to the list by clicking the shopping cart button or pressing the Enter key.
+2. The new item can not be an empty string and in that case nothing happens.
+3. You can delete an item from the list by clicking the trash button. A smooth animation is triggered
+when this action happens.
+4. You can toggle the cross out event to an item to indicate that this item was purchased by clicking the check button.
+5. Date is also added at the top of the list and shows the current date.
+6. Local storage functionality is also added so when you refresh the page the previous list is shown.
+
+Live DEMO: https://chridas.github.io/grocery-list
---
+++ grocery.js
@@ -0,0 +1,102 @@
+// Selectors
+const dateDiv = document.querySelector(".date");
+const itemInput = document.querySelector(".item-input");
+const itemButton = document.querySelector(".item-button");
+const groceryList = document.querySelector(".grocery-list");
+let items = [];
+
+// Date
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const date = new Date();
+dateDiv.innerHTML = `${date.getDate()} ${months[date.getMonth()]}, ${date.getFullYear()}`;
+
+// Event listeners
+document.addEventListener("DOMContentLoaded", getLocalItem);
+itemButton.addEventListener("click", addItem);
+groceryList.addEventListener("click", areaClicked);
+itemInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    addItem();
+  }
+});
+
+//Functions
+function createDiv(item) {
+  if (item.length > 0) {
+    const itemDiv = document.createElement("div");
+    itemDiv.classList.add("item-div");
+    itemDiv.innerHTML = `<li class="item-list">${item}</li>
+                         <button class="check-btn"><i class='fas fa-check'></i></button>
+                         <button class="delete-btn"><i class='fas fa-trash'></i></button>`;
+    groceryList.appendChild(itemDiv);
+  }
+}
+
+function addItem() {
+  const newItem = itemInput.value;
+  createDiv(newItem);
+  saveLocalItem(newItem);
+  itemInput.value = "";
+}
+
+function deleteItem(item) {
+  item.classList.add("fall");
+  item.addEventListener("transitionend", function () {
+    this.remove();
+  });
+}
+
+function checkItem(item) {
+  item.classList.toggle("completed");
+}
+
+function areaClicked(e) {
+  const item = e.target;
+  const itemList = item.parentElement;
+  const classItem = item.classList[0];
+
+  if (classItem === "delete-btn") {
+    deleteItem(itemList);
+    removeLocalItem(itemList);
+  } else if (classItem === "check-btn") {
+    checkItem(itemList);
+  }
+}
+
+function checkStorage() {
+  const isLocaleStorageEmpty = localStorage.getItem("items") === null;
+  if (!isLocaleStorageEmpty) {
+    items = JSON.parse(localStorage.getItem("items"));
+  }
+}
+
+function saveLocalItem(item) {
+  checkStorage();
+  items.push(item);
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
+function getLocalItem() {
+  checkStorage();
+  items.forEach(createDiv);
+}
+
+function removeLocalItem(item) {
+  checkStorage();
+  const itemIndex = item.children[0].innerText;
+  items.splice(items.indexOf(itemIndex), 1);
+  localStorage.setItem("items", JSON.stringify(items));
+}
